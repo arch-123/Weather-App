3952aaa9e91c88b5f7f92443b576855607d3bac9
Archana Gujar <archanasgujar@gmail.com>
2023-03-16 01:44:15 +0530

    added Foreast Functionality and code modification

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 from flask import Flask, render_template, request
 import requests, json
 from config import urls
+from datetime import datetime
+import pandas as pd
 
 app = Flask(__name__)
 
-
-
-
 def get_lat_lon(city_name):
-    location_url = (
-        urls["location"]["base_url"] 
-        + "q=" + city_name 
-        + "&limit=" + urls["location"]["limit"] 
-        + "&appid=" + urls["api_key"]
-    )
+    location_url = urls["location"]["base_url"].format(city_name, urls["location"]["limit"], urls["api_key"])
     response = requests.get(location_url)
     lat, lon = 0, 0
     if response.status_code == 200:
@@ -23,39 +17,103 @@ def get_lat_lon(city_name):
     return lat, lon
 
 
-def clean_api_response(x):
+def kelvin_to_celcius(temp):
+    return str(round(temp - 273.15)) + " ℃";
+
+
+def clean_current_weather_api_response(x):
     data = {}
-    data['Weather'] = x['weather'][0]['main']
+    data['Weather'] = x['weather'][0]['description'].capitalize()
     data['Temperature'] = kelvin_to_celcius(x['main']['temp'])
     data['Temperature(feels like)'] = kelvin_to_celcius(x['main']['feels_like'])
     data['Min Temperature'] = kelvin_to_celcius(x['main']['temp_min'])
     data['Max Temperature'] = kelvin_to_celcius(x['main']['temp_max'])
     data['Humidity'] = str(x['main']['humidity']) + " %"
-    data['Visibility'] = str(x['visibility']) + " meter"
+    data['Visibility'] = str(round(x['visibility']/1000, 1)) + " kms"
     data['Pressure'] = str(x['main']['pressure']) + " hPa"
-    data['Wind Speed'] = str(x['wind']['speed']) + " meter/sec"
+    data['Wind Speed'] = str(round(x['wind']['speed']*3.6, 1)) + " km/hr"
+    return data
+
+
+def get_current_weather(lat, lon):
+    url = urls["current_weather"]["base_url"].format(str(lat), str(lon), urls["api_key"])
+    response = requests.get(url)
+    if response.status_code == 200:
+        x = response.json()
+        # print(x)
+    data = clean_current_weather_api_response(x)
+    return data
+
+
+def hour_format_converter_24_to_12(df):
+    def check_hour(x):
+        if x>12: return str(x-12) + " PM"
+        if x==12: return str(x) + " PM"
+        if x==0: return "12 AM"
+        return str(x) + " AM"
+    return df.astype(int).apply(lambda x: check_hour(x))
+
+
+def clean_future_weather_api_response(res):
+    for i in range(len(res['list'])):
+        res['list'][i]['weather'] = res['list'][i]['weather'][0]
+    df = pd.json_normalize(res['list'])
+    df['dt'] = df['dt'].apply(lambda i: datetime.utcfromtimestamp(i).strftime("%d %b-%H"))
+    df[['Date', 'Hour']] = df['dt'].str.split("-", expand = True)
+    df['Hour'] = hour_format_converter_24_to_12(df['Hour'])
+    df = df[[
+        'Date', 'Hour', 'weather.description', 'main.temp', 'main.feels_like', 'main.temp_min', 
+        'main.temp_max', 'main.humidity', 'visibility', 'main.pressure', 'wind.speed'
+    ]]
+    df = df.rename(columns = {
+        'weather.description': "Weather",
+        'main.temp': "Temperature", 
+        'main.feels_like': "Temperature(feels like)", 
+        'main.temp_min': "Min Temperature", 
+        'main.temp_max': "Max Temperature", 
+        'main.humidity': "Humidity",
+        'visibility': "Visibility",
+        'main.pressure': "Pressure", 
+        'wind.speed': "Wind Speed"
+    })
+    df['Weather'] = df['Weather'].astype(str).str.capitalize()
+    df['Temperature'] = df['Temperature'].apply(lambda x: kelvin_to_celcius(x))
+    df['Temperature(feels like)'] = df['Temperature(feels like)'].apply(lambda x: kelvin_to_celcius(x))
+    df['Min Temperature'] = df['Min Temperature'].apply(lambda x: kelvin_to_celcius(x))
+    df['Max Temperature'] = df['Max Temperature'].apply(lambda x: kelvin_to_celcius(x))
+    df['Humidity'] = df['Humidity'].astype(str) + " %"
+    df['Visibility'] = (df['Visibility']/1000).astype(str) + " kms"
+    df['Pressure'] = df['Pressure'].round(1).astype(str) + " hPa"
+    df['Wind Speed'] = (df['Wind Speed']*3.6).round(1).astype(str) + " km/hr"
+    return df
+
+
+def get_future_weather(lat, lon):
+    url = urls["forecast"]["base_url"].format(str(lat), str(lon), urls["api_key"])
+    response = requests.get(url)
+    if response.status_code == 200:
+        x = response.json()
+        # print(x)
+    data = clean_future_weather_api_response(x)
     return data
 
 
 @app.route("/handlesubmit", methods=['POST'])
-def get_current_weather():
+def handleSubmit():
     city_name = request.form['city']
-    lat, lon = get_lat_lon(city_name)
-    current_weather_url = (
-        urls["current_weather"]["base_url"]
-        + "lat=" + str(lat)
-        +"&lon=" + str(lon)
-        + "&appid=" + urls["api_key"]
+    try:
+        lat, lon = get_lat_lon(city_name)
+    except:
+        return render_template("index.html", error = True, city=city_name)
+    current_weather = get_current_weather(lat, lon)
+    future_weather = get_future_weather(lat, lon)
+    return render_template(
+        "weather.html", 
+        city=city_name, 
+        current_weather=current_weather, 
+        future_weather = future_weather,
+        error = False
     )
-    response = requests.get(current_weather_url)
-    if response.status_code == 200:
-        x = response.json()
-        print(x)
-    data = clean_api_response(x)
-    return render_template("weather.html", weather=data, city=city_name)
-
-def kelvin_to_celcius(temp):
-    return str(round(temp - 273.15)) + " ℃";
 
 
 @app.route("/")
--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 urls = {
-    "api_key": "131972c0062af58200b788eafc52e775",
+    "api_key": "API_KEY",
     "location": {
-    "base_url": "http://api.openweathermap.org/geo/1.0/direct?",
+    "base_url": "http://api.openweathermap.org/geo/1.0/direct?q={}&limit={}&appid={}",
     "limit" : "5",
     "city_name": "Mumbai"
     },
     "current_weather": {
-    "base_url": "https://api.openweathermap.org/data/2.5/weather?"
+    "base_url": "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}"
+    },
+    "forecast": {
+    "base_url": "https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}"
     }
 }
